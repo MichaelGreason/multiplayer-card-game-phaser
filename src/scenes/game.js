615dc99b26600ce145383a5cb56b4af550457ab4
Michael Greason <mgreason18@gmail.com>
2023-08-07 14:27:43 -0400

    cards are movable and placeabble

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -18,7 +18,8 @@ create() {
 
     this.zone = new Zone(this);
     this.dropZone = this.zone.renderZone();
-    this.outline = this.zone.renderOutline(this.dropZone)
+    this.outline = this.zone.renderOutline(this.dropZone);
+
 
     this.dealCards = () => {
         for (let i = 0; i < 5; i++) {
@@ -40,12 +41,32 @@ create() {
     this.dealText.on('pointerout', function () {
             self.dealText.setColor('#00ffff')
         })
+
+    this.input.on('dragstart', function (pointer, gameObject,) {
+        gameObject.setTint(0xff69b4);
+        self.children.bringToTop(gameObject);
+    })
+
+    this.input.on('dragend', function (pointer, gameObject, dropped) {
+        gameObject.setTint();
+        if (!dropped) {
+            gameObject.x = gameObject.input.dragStartX;
+            gameObject.y = gameObject.input.dragStartY;
+        }
+    })
+
     
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY,) {
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
         gameObject.x = dragX;
         gameObject.y = dragY;
     })
-
+    
+    this.input.on('drop', function (pointer, gameObject, dropZone) {
+        dropZone.data.values.cards++;
+        gameObject.x = (dropZone.x - 250) + (dropZone.data.values.cards * 75);
+        gameObject.y = dropZone.y;
+        gameObject.disableInteractive()
+    })
 }
 update() {
 
