17786cea228678d221685298312a48795cae41bf
Michael Greason <mgreason18@gmail.com>
2023-08-07 13:34:46 -0400

    constructing game 1 set dela text

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -5,9 +5,35 @@ constructor() {
     })
 }
 preload() {
-
+    this.load.image("cyanCardFront", "src/assets/CyanCardFront.png");
+    this.load.image("cyanCardBack", "src/assets/CyanCardBack.png");
+    this.load.image("magentaCardFront", "src/assets/MagentaCardFront.png");
+    this.load.image("magentaCardBack", "src/assets/MagentaCardBack.png");
 }
 create() {
+    let self = this;
+
+    this.card = this.add.image(300, 300, 'cyanCardFront').setScale(.4, .4).setInteractive();
+
+    this.input.setDraggable(this.card);
+
+    this.dealCards = () => {
+
+    }
+
+    this.dealText = this.add.text(75, 350, ['DEAL CARDS']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#00ffff').setInteractive();
+
+    this.dealText.on('pointerdown', function() {
+        self.dealCards()
+    })
+
+    this.dealText.on('pointerover', function () {
+            self.dealText.setColor('#ff69b4')
+        })
+
+    this.dealText.on('pointerout', function () {
+            self.dealText.setColor('#00ffff')
+        })
 
 }
 update() {
