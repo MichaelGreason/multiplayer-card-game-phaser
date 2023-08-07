4e06e29ca1f91c8ecd96e86f81dec46b8b26a5af
Michael Greason <mgreason18@gmail.com>
2023-08-07 15:08:32 -0400

    server setup done

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,6 +1,6 @@
 import Card from "../helpers/card";
 import Zone from "../helpers/zone";
-import io from 'socket.io/client';
+import io from 'socket.io-client';
 
 export default class Game extends Phaser.Scene {
 constructor() {
@@ -22,7 +22,7 @@ create() {
     this.outline = this.zone.renderOutline(this.dropZone);
 
 
-    this.socket = io('http://localhost3000')
+    this.socket = io(`http://localhost:3000`)
 
     this.socket.on('connect', function() {
         console.log('Connected!');
